8db762185c943a97375723baf53f7215437b8305
git-user-cpp <akushyk799@gmail.com>
2023-06-22 16:05:58 +0300

    GUI was modified

--- src/desktop/impl_options/information.rs
+++ src/desktop/impl_options/information.rs
@@ -23,37 +23,38 @@ use dioxus::prelude::*;
 pub fn information(cx: Scope) -> Element {
 	render! {
 		rsx! {
-			div {
-				h1 {
-						style: "color: red",
-						"Information about the program"
-				}
+			style {include_str!("../style/dark_theme.css")},
+
+			h1 {
+				style: "color: red",
+				"Information about the program"
 			}
 
-			div {
-				p {
-					"Copyright 2023 Andrew Kushyk"
-				}
+			p {
+				style: "color: white",
+				"Copyright 2023 Andrew Kushyk"
+			}
 
-				p {
-					"Licensed under the Apache License, Version 2.0 (the \"License\");
-					   you may not use this file except in compliance with the License.
-					   You may obtain a copy of the License at"
-				}
+			p {
+				style: "color: white",
+				"Licensed under the Apache License, Version 2.0 (the \"License\");
+				   you may not use this file except in compliance with the License.
+				   You may obtain a copy of the License at"
+			}
 
-				a {
-					style: "color: red",
-					href: "http://www.apache.org/licenses/LICENSE-2.0",
-					"http://www.apache.org/licenses/LICENSE-2.0"
-				}
+			a {
+				style: "color: red",
+				href: "http://www.apache.org/licenses/LICENSE-2.0",
+				"http://www.apache.org/licenses/LICENSE-2.0"
+			}
 
-				p {
-					"Unless required by applicable law or agreed to in writing, software
-					distributed under the License is distributed on an \"AS IS\" BASIS,
-					WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-					See the License for the specific language governing permissions and
-					limitations under the License."
-				}
+			p {
+				style: "color: white",
+				"Unless required by applicable law or agreed to in writing, software
+				distributed under the License is distributed on an \"AS IS\" BASIS,
+				WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+				See the License for the specific language governing permissions and
+				limitations under the License."
 			}
 		}
 	}
--- src/desktop/impl_options/products.rs
+++ src/desktop/impl_options/products.rs
@@ -23,11 +23,11 @@ use dioxus::prelude::*;
 pub fn insert_product(cx: Scope) -> Element {
 	render! {
 		rsx! {
-			div {
-				h1 {
-						style: "color: red",
-						"Need to be implemented"
-				}
+			style {include_str!("../style/dark_theme.css")},
+
+			h1 {
+				style: "color: red",
+				"Need to be implemented"
 			}
 		}
 	}
